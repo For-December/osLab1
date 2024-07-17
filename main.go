ace68ebfdcd9f11e7d346b789021612162dca6bb
fordecember <1921567337@qq.com>
2024-07-17 20:39:06 +0800

    update: 控制台输出

--- main.go
+++ main.go
@@ -9,10 +9,11 @@ func main() {
 	processes := GenerateProcesses()
 
 	timeSlice := 2
+	fmt.Println("############ 时间片轮转 ############")
 	algorithm.RoundRobin(processes, timeSlice)
-	fmt.Println("############")
+	fmt.Println("############ 短作业优先 ############")
 	algorithm.ShortestJobFirst(processes)
 
-	fmt.Println("############")
+	fmt.Println("############ 多级反馈队列 ############")
 	algorithm.MultilevelFeedbackQueue(processes, []int{1, 2, 4})
 }
